53d35b103fe3439b1c74844d7b5facaf961dd016
Cameron Kirby <cameronevankirby@gmail.com>
2020-06-08 15:52:38 -0500

    Added MVP

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.css';
+
+import TodoList from './components/TodoList'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
